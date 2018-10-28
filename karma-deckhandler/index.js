fe4ea8741a26852fd8e72d27c8574013b387cae2
Paul Fox <paulf.ratinox@gmail.com>
2018-10-28 23:15:04 +0000

    Adds cards array builder

--- karma-deckhandler/index.js
+++ karma-deckhandler/index.js
@@ -3,6 +3,9 @@ const Firestore = require('@google-cloud/firestore')
 // Firestore API
 const firestore = new Firestore({timestampsInSnapshots: true});
 
+const suits = ['h', 'd', 's', 'c']
+const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 exports.karma_deckhandler = function(request, response) {
     if (request.method === "OPTIONS") {
         console.log('origins')
@@ -12,4 +15,15 @@ exports.karma_deckhandler = function(request, response) {
         response.set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-Width, Origin, Accept, X-Custom-Header')
         response.status(200).send()
     }    
+}
+
+function makeCardArray () {
+    let cards = []
+    for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
+        for (let rankIndex = 0; rankIndex < ranks.length; rankIndex++) {
+            cards.push[`${suits[suitIndex]}${ranks[rankIndex]}`]
+        }
+    }
+    console.log(cards)
+    return cards
 }
